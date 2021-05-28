92ecb1d4cf8fa23f18944c3466ae320259dd2717
iolkos <pbegkas98@outlook.com>
2021-05-28 20:43:30 +0300

    First reader commit
    added POI lib and first few bits to the WordReader class

--- TextToSpeech/src/input/WordReader.java
+++ TextToSpeech/src/input/WordReader.java
@@ -1,12 +1,27 @@
 package input;
 
 import java.util.List;
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.xwpf.extractor.XPFFWordExtracto
+
 
 public class WordReader implements DocumentReader
 {
+	
+	
+	WordReader(String path)
+	{
+		
+		File file = new File(path);
+		FileInputStream fis = new FileInputStream(file);
+		
+	}
 
 	@Override
-	public List<String> read() {
+	public List<String> read() 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
